14b77520cf9697176813713bd31ceeb9f12c36ba
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-02 04:55:26 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,8 @@
-# Write your code here
+count = 10   
+while count > 0 do 
+  break if count == 0  
+  puts "#{count}" 
+  count = count - 1 
+  end
+  count == 0 
+  puts "Happy New Year!"# Write your code here
